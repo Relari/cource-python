f34dae0dd2f918572b748beaeb047607ae974b8d
Renzo Daniel Lavado Rivas <rlavador@emeal.nttdata.com>
2022-05-20 16:21:28 -0500

    updated directory

--- com/pe/relari/demo/java/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.pe.relari.demo.java;
-
-public class Demo {
-    public static void main(String [] agrs) {
-
-        var name = "Renzo";
-        System.out.println("Hello ".concat(name));
-    }
-}
