b373e4bd10a5f5b25f2b2512fdf712e52ae3e154
Relari <renzolavador@gmail.com>
2025-01-05 19:25:55 -0500

    refactor[Variable]: Mensajes de los print actualizados.

--- Variable.py
+++ Variable.py
@@ -2,13 +2,23 @@
 
 # Variable de Tipo String
 nombre = "Renzo"
-print(nombre)
+print(type(nombre))
+mensaje = f"Bienvenido {nombre}"
+print(mensaje)
 
 # Variable de Tipo Integer
 edad = 25
-print(edad)
+print(type(edad))
+mensaje = f"Tienes {edad} anios."
+print(mensaje)
+
+peso = 84.4
+print(type(peso))
+mensaje = f"Tienes {peso} kilos."
+print(mensaje)
 
 # Variable de Tipo Boolean
 esActivo = True
-
-print(esActivo)
+print(type(esActivo))
+mensaje = f"Esta activo: {esActivo}"
+print(mensaje)
